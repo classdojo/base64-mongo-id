565c06ed0e6eecf85abc4bf587fbd9899828b2b7
Peter Hayes <peter.k.hayes@gmail.com>
2016-01-06 17:22:56 -0800

    changed encoding system to better match spec.  added more tests.  changed method names

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
-var numsToChars = {0:"0",1:"1",2:"2",3:"3",4:"4",5:"5",6:"6",7:"7",8:"8",9:"9",10:"a",11:"b",12:"c",13:"d",14:"e",15:"f",16:"g",17:"h",18:"i",19:"j",20:"k",21:"l",22:"m",23:"n",24:"o",25:"p",26:"q",27:"r",28:"s",29:"t",30:"u",31:"v",32:"w",33:"x",34:"y",35:"z",36:"A",37:"B",38:"C",39:"D",40:"E",41:"F",42:"G",43:"H",44:"I",45:"J",46:"K",47:"L",48:"M",49:"N",50:"O",51:"P",52:"Q",53:"R",54:"S",55:"T",56:"U",57:"V",58:"W",59:"X",60:"Y",61:"Z",62:"_",63:"-"}
-var charsToNums = {"0":0,"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"a":10,"b":11,"c":12,"d":13,"e":14,"f":15,"g":16,"h":17,"i":18,"j":19,"k":20,"l":21,"m":22,"n":23,"o":24,"p":25,"q":26,"r":27,"s":28,"t":29,"u":30,"v":31,"w":32,"x":33,"y":34,"z":35,"A":36,"B":37,"C":38,"D":39,"E":40,"F":41,"G":42,"H":43,"I":44,"J":45,"K":46,"L":47,"M":48,"N":49,"O":50,"P":51,"Q":52,"R":53,"S":54,"T":55,"U":56,"V":57,"W":58,"X":59,"Y":60,"Z":61,"_":62,"-":63}
+var numsToChars64 = {0:"A",1:"B",2:"C",3:"D",4:"E",5:"F",6:"G",7:"H",8:"I",9:"J",10:"K",11:"L",12:"M",13:"N",14:"O",15:"P",16:"Q",17:"R",18:"S",19:"T",20:"U",21:"V",22:"W",23:"X",24:"Y",25:"Z",26:"a",27:"b",28:"c",29:"d",30:"e",31:"f",32:"g",33:"h",34:"i",35:"j",36:"k",37:"l",38:"m",39:"n",40:"o",41:"p",42:"q",43:"r",44:"s",45:"t",46:"u",47:"v",48:"w",49:"x",50:"y",51:"z",52:"0",53:"1",54:"2",55:"3",56:"4",57:"5",58:"6",59:"7",60:"8",61:"9",62:"_",63:"-"};
+var numsToChars16 = {0:"0",1:"1",2:"2",3:"3",4:"4",5:"5",6:"6",7:"7",8:"8",9:"9",10:"a",11:"b",12:"c",13:"d",14:"e",15:"f"};
+var charsToNums64 = {"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7,"I":8,"J":9,"K":10,"L":11,"M":12,"N":13,"O":14,"P":15,"Q":16,"R":17,"S":18,"T":19,"U":20,"V":21,"W":22,"X":23,"Y":24,"Z":25,"a":26,"b":27,"c":28,"d":29,"e":30,"f":31,"g":32,"h":33,"i":34,"j":35,"k":36,"l":37,"m":38,"n":39,"o":40,"p":41,"q":42,"r":43,"s":44,"t":45,"u":46,"v":47,"w":48,"x":49,"y":50,"z":51,"0":52,"1":53,"2":54,"3":55,"4":56,"5":57,"6":58,"7":59,"8":60,"9":61,"_":62,"-":63};
 
 function parse2DigitInt64 (str) {
-  return 64 * charsToNums[str[0]] + charsToNums[str[1]];
+  return 64 * charsToNums64[str[0]] + charsToNums64[str[1]];
 }
 
-var longRegex = /^[0-9a-f]{24}$/;
-var shortRegex = /^[0-9a-zA-Z-_]{16}$/;
+var hexRegex24 = /^[0-9a-f]{24}$/;
+var base64Regex16 = /^[0-9a-zA-Z-_]{16}$/;
 
 module.exports = {
   
-  longToShort: function (long) {
-    if (!longRegex.test(long)) {
-      throw new Error("`base64-mongo-id.longToShort` must receive an input matching " + longRegex.toString());
+  toBase64: function (hex) {
+    hex = String(hex);
+    if (!hexRegex24.test(hex)) {
+      throw new Error("`base64-mongo-id.toBase64` must receive an input matching " + hexRegex24.toString());
     }
     var output = [];
-    for (var i = 0; i < long.length; i += 3) {
-      var slice = long.slice(i, i + 3);
+    for (var i = 0; i < hex.length; i += 3) {
+      var slice = hex.slice(i, i + 3);
       var num = parseInt(slice, 16);
       var firstDigit = Math.floor(num / 64);
       var secondDigit = num % 64;
-      output.push(numsToChars[firstDigit], numsToChars[secondDigit]);
+      output.push(numsToChars64[firstDigit], numsToChars64[secondDigit]);
     }
     return output.join("");
   },
 
-  shortToLong: function (short) {
-    if (!shortRegex.test(short)) {
-      throw new Error("`base64-mongo-id.shortToLong` must receive an input matching " + shortRegex.toString());
+  toHex: function (base64) {
+    base64 = String(base64);
+    if (!base64Regex16.test(base64)) {
+      throw new Error("`base64-mongo-id.toHex` must receive an input matching " + base64Regex16.toString());
     }
     var output = [];
-    for (var i = 0; i < short.length; i += 2) {
-      var slice = short.slice(i, i + 2);
+    for (var i = 0; i < base64.length; i += 2) {
+      var slice = base64.slice(i, i + 2);
       var num = parse2DigitInt64(slice);
       var firstDigit = Math.floor(num / 256);
       var secondDigit = Math.floor(num / 16) % 16;
       var thirdDigit = num % 16;
-      output.push(numsToChars[firstDigit], numsToChars[secondDigit], numsToChars[thirdDigit]);
+      output.push(numsToChars16[firstDigit], numsToChars16[secondDigit], numsToChars16[thirdDigit]);
     }
     return output.join("");
   }
--- test.js
+++ test.js
@@ -2,39 +2,43 @@ var convertId = require("./index");
 var expect = require("expect.js");
 
 var data = [
-  {long: "521fc843178a921652000007", short: "kx-8gNuaAxpi0007"},
-  {long: "521fc843178a921652000060", short: "kx-8gNuaAxpi001w"},
-  {long: "52336be5b74a7a0000000015", short: "kzdHVrtauw00000l"},
-  {long: "53c5d28ca62be0a940000002", short: "kYnizaoHUaB00002"},
-  {long: "521fc86d178a92165200001d", short: "kx-8rhuaAxpi000t"},
-  {long: "52af5b7913d8e99f41136d4c", short: "kGZruhfoWpZ14SRc"},
-  {long: "5397b40c7232ef916a000967", short: "kVuQ378OXV5G00BD"},
+  {hex: "521fc843178a921652000007", base64: 'Uh-IQxeKkhZSAAAH'},
+  {hex: "521fc843178a921652000060", base64: 'Uh-IQxeKkhZSAABg'},
+  {hex: "52336be5b74a7a0000000015", base64: 'UjNr5bdKegAAAAAV'},
+  {hex: "53c5d28ca62be0a940000002", base64: 'U8XSjKYr4KlAAAAC'},
+  {hex: "521fc86d178a92165200001d", base64: 'Uh-IbReKkhZSAAAd'},
+  {hex: "52af5b7913d8e99f41136d4c", base64: 'Uq9beRPY6Z9BE21M'},
+  {hex: "5397b40c7232ef916a000967", base64: 'U5e0DHIy75FqAAln'},
+  {hex: "000000000000000000000000", base64: 'AAAAAAAAAAAAAAAA'},
+  {hex: "000000000000000000000003", base64: 'AAAAAAAAAAAAAAAD'},
+  {hex: "ffffffffffffffffffffffff", base64: '----------------'},
 ];
 
 describe("base64-mongo-id", function() {
 
-  it("throws on bad input for long to short", function() {
+  it("throws on bad input for hex to base64", function() {
     expect(function() {
-      convertId.longToShort(data[0].short);
+      convertId.toBase64(data[0].base64);
     }).to.throwError();
   });
 
-  it("throws on bad input for short to long", function() {
+  it("throws on bad input for base64 to hex", function() {
     expect(function() {
-      convertId.shortToLong(data[0].long);
+      convertId.toHex(data[0].hex);
     }).to.throwError();
   });
 
   data.forEach(function(pair, i) {
     
-    it("converts long to short properly - test " + (i + 1), function() {
-      expect(convertId.longToShort(pair.long)).to.be(pair.short);
+    it("converts hex to base64 properly - test " + (i + 1), function() {
+      expect(convertId.toBase64(pair.hex)).to.be(pair.base64);
     });
 
-    it("converts short to long properly - test " + (i + 1), function() {
-      expect(convertId.shortToLong(pair.short)).to.be(pair.long);
+    it("converts base64 to hex properly - test " + (i + 1), function() {
+      expect(convertId.toHex(pair.base64)).to.be(pair.hex);
     });
 
   });
 
 });
+
